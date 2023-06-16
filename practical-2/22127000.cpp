7830fd802f494fdc9a37028b80b0d620e2e45966
Phu Duc Nguyen <sillycat@duck.com>
2023-06-16 14:10:06 +0700

    Fix some logic and clean code

--- practical-2/22127000.cpp
+++ practical-2/22127000.cpp
@@ -1,52 +1,324 @@
-#include "func.cpp"
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
 
-void todo(string flightdata_filename, string data_filename){
-    // Create an instance of the List structure
-    List flightList;
-    flightList.head = nullptr;
-    flightList.tail = nullptr;
+using namespace std;
 
-    // Read data from the file and populate the double linked list
-    ifstream file(flightdata_filename);
+struct Flight {
+    string source;
+    string destination;
+    int hour;
+    int minute;
+};
 
-    if (!file.is_open()) {
-        cout << "Failed to open the file." << endl;
+struct Node {
+    Node* next;
+    Node* prev;
+    Flight data;
+};
+
+struct DoublyLinkedList {
+    Node* head;
+    Node* tail;
+};
+
+// Create a new node with the given flight data
+Node* createNode(Flight fl) {
+    Node* node = new Node;
+    node->data = fl;
+    node->next = node->prev = nullptr;
+    return node;
+}
+
+// Create an empty doubly linked list
+void createList(DoublyLinkedList& L) {
+    L.head = L.tail = nullptr;
+}
+
+// Add a new node to the head of the list
+void addHead(DoublyLinkedList& L, Flight data) {
+    Node* node = createNode(data);
+    if (L.head == nullptr)
+        L.head = L.tail = node;
+    else {
+        node->next = L.head;
+        L.head->prev = node;
+        L.head = node;
+    }
+}
+
+// Add a new node to the tail of the list
+void addTail(DoublyLinkedList& L, Flight data) {
+    Node* node = createNode(data);
+    if (L.tail == nullptr)
+        L.head = L.tail = node;
+    else {
+        L.tail->next = node;
+        node->prev = L.tail;
+        L.tail = node;
+    }
+}
+
+// Remove the head node of the list
+void removeHead(DoublyLinkedList& L) {
+    if (L.head == nullptr)
+        return;
+
+    if (L.head == L.tail) {
+        delete L.head;
+        L.head = L.tail = nullptr;
         return;
     }
 
-    // Process now
-    string line;
-    while (getline(file, line)) {
-        // Process each line here
-        size_t firstQuotePos = line.find('"');
-        size_t lastQuotePos = line.rfind('"');
-        size_t hoursPos = line.find(" hours");
-        size_t minsPos = line.find(" minutes");
-
-        string tempData = line.substr(firstQuotePos + 1, lastQuotePos - firstQuotePos - 1);
-        // string src = line.substr(2, commaPos - 2);
-        // string des = line.substr(commaPos + 1, (spacePos / 2) - 1);
-        
-
-        // Testing
-        // cout << firstQuotePos << "," << lastQuotePos << endl;
-        cout << tempData << endl;
-    }
-
-    file.close();
-
-    // Print the flights in the list
-    printlist(flightList);
-
-    // Clean up the flight list
-    deleverything_list(flightList);
+    Node* temp = L.head;
+    L.head = L.head->next;
+    L.head->prev = nullptr;
+    delete temp;
 }
 
-void main_debug(){
-    string flightdata_filename = "g1.jl";
+// Remove the tail node of the list
+void removeTail(DoublyLinkedList& L) {
+    if (L.head == nullptr)
+        return;
 
-    // Provide the data_filename if needed
-    string data_filename = "";
+    if (L.head == L.tail) {
+        delete L.head;
+        L.head = L.tail = nullptr;
+        return;
+    }
 
-    todo(flightdata_filename, data_filename);
+    Node* temp = L.tail;
+    L.tail = L.tail->prev;
+    L.tail->next = nullptr;
+    delete temp;
+}
+
+// Compare two flight objects for equality
+bool compareFlights(Flight f1, Flight f2) {
+    return (f1.destination == f2.destination &&
+            f1.source == f2.source &&
+            f1.hour == f2.hour &&
+            f1.minute == f2.minute);
+}
+
+// Remove the node before the given flight in the list
+void removeBefore(DoublyLinkedList& l, Flight fl) {
+    if (l.head == nullptr || l.head == l.tail) {
+        return;
+    }
+
+    Node* current = l.head;
+    while (current && !compareFlights(current->data, fl)) {
+        current = current->next;
+    }
+
+    if (current == nullptr || current == l.head) {
+        return;
+    }
+
+    Node* prevCurrent = current->prev;
+    if (prevCurrent == l.head) {
+        removeHead(l);
+        return;
+    }
+
+    current->prev = prevCurrent->prev;
+    prevCurrent->prev->next = current;
+    delete prevCurrent;
+}
+
+// Remove the node after the given flight in the list
+void removeAfter(DoublyLinkedList& l, Flight fl) {
+    if (l.head == nullptr || l.head == l.tail) {
+        return;
+    }
+
+    for (Node* p = l.head; p; p = p->next) {
+        if (compareFlights(p->data, fl)) {
+            if (p == l.tail)
+                return;
+            else {
+                Node* temp = p->next;
+                if (temp == l.tail) {
+                    removeTail(l);
+                    return;
+                }
+                p->next = temp->next;
+                temp->next->prev = p;
+                delete temp;
+                return;
+            }
+        }
+    }
+}
+
+// Add a new node after the given flight in the list
+bool addAfter(DoublyLinkedList& L, Flight f1, Flight f2) {
+    Node* head = L.head;
+    while (head) {
+        if (compareFlights(head->data, f1)) {
+            if (head->next == nullptr) {
+                addTail(L, f1);
+                return true;
+            }
+            Node* node = createNode(f2);
+            Node* tmp = head->next;
+            node->next = tmp;
+            tmp->prev = node;
+            head->next = node;
+            node->prev = head;
+            return true;
+        }
+        head = head->next;
+    }
+    return false;
+}
+
+// Add a new node before the given flight in the list
+bool addBefore(DoublyLinkedList& L, Flight f1, Flight f2) {
+    if (L.head == nullptr) {
+        return false;
+    }
+
+    if (compareFlights(L.head->data, f1))
+        addHead(L, f2);
+    else {
+        Node* curr = L.head;
+        while (curr->next && !compareFlights(curr->next->data, f1))
+            curr = curr->next;
+        if (!curr->next)
+            return false;
+        Node* temp = createNode(f2);
+        temp->prev = curr;
+        temp->next = curr->next;
+        curr->next = temp;
+        temp->next->prev = temp;
+    }
+    return true;
+}
+
+// Add a new node at the specified position in the list
+bool addAtPosition(DoublyLinkedList& L, Flight data, int pos) {
+    if (pos < 0)
+        return false;
+
+    if (pos == 0)
+        addHead(L, data);
+    else {
+        Node* q = L.head;
+        int idx = 1;
+        while (idx < pos) {
+            idx++;
+            q = q->next;
+            if (q == nullptr)
+                return false;
+        }
+        Node* temp = createNode(data);
+        if (q == L.tail)
+            addTail(L, data);
+        else {
+            temp->next = q->next;
+            q->next->prev = temp;
+            temp->prev = q;
+            q->next = temp;
+        }
+    }
+    return true;
+}
+
+// Print the details of a flight
+void printFlight(Flight fl) {
+    cout << "Source: " << fl.source << ", Destination: " << fl.destination
+         << ", Hour: " << fl.hour << ", Minute: " << fl.minute << endl;
+}
+
+// Print the flights in the doubly linked list
+void printDoublyLinkedList(DoublyLinkedList L) {
+    for (Node* p = L.head; p; p = p->next) {
+        printFlight(p->data);
+    }
+}
+
+// Extract flight details from a string line
+Flight extractFlight(string line) {
+    stringstream s(line);
+    Flight f;
+
+    string buf;
+    getline(s, buf, '\"');
+    getline(s, f.source, ',');
+    getline(s, f.destination, '\"');
+
+    getline(s, buf, '\"');
+    getline(s, buf, ',');
+    
+    // Notice: Have bug what: stoi() here, when using stoi() function from <string> library
+    // So i use atoi instead of...
+    f.hour = atoi(buf.c_str());
+
+    getline(s, buf, '\"');
+    f.minute = atoi(buf.c_str());
+    // cout << f.minute << endl;
+
+    return f;
+}
+
+int main() {
+    DoublyLinkedList flightsList;
+    createList(flightsList);
+
+    ifstream inputFile("g1.jl");
+    if (inputFile.is_open()) {
+        string line;
+        while (getline(inputFile, line)) {
+            Flight f = extractFlight(line);
+            addTail(flightsList, f);
+        }
+        inputFile.close();
+    }
+
+    // // Remove a flight before the given flight
+    // Flight beforeFlight{"SGN", "DAD", 10, 30};
+    // cout << "Removing a flight before the given flight: ";
+    // removeBefore(flightsList, beforeFlight);
+    printDoublyLinkedList(flightsList);
+
+    // // Remove a flight after the given flight
+    // Flight afterFlight{"SGN", "HAN", 8, 45};
+    // cout << "Removing a flight after the given flight: ";
+    // removeAfter(flightsList, afterFlight);
+    // printDoublyLinkedList(flightsList);
+
+    // // Add a new flight after the given flight
+    // Flight addAfterFlight{"SGN", "HAN", 9, 0};
+    // Flight newFlight{"SGN", "DAD", 11, 0};
+    // cout << "Adding a new flight after the given flight: ";
+    // bool success = addAfter(flightsList, addAfterFlight, newFlight);
+    // if (success)
+    //     printDoublyLinkedList(flightsList);
+    // else
+    //     cout << "Failed to add a new flight." << endl;
+
+    // // Add a new flight before the given flight
+    // Flight addBeforeFlight{"SGN", "DAD", 12, 0};
+    // Flight newFlight2{"SGN", "HAN", 10, 0};
+    // cout << "Adding a new flight before the given flight: ";
+    // success = addBefore(flightsList, addBeforeFlight, newFlight2);
+    // if (success)
+    //     printDoublyLinkedList(flightsList);
+    // else
+    //     cout << "Failed to add a new flight." << endl;
+
+    // // Add a new flight at a specific position
+    // Flight addAtPosFlight{"SGN", "HAN", 11, 30};
+    // int position = 2;
+    // cout << "Adding a new flight at position " << position << ": ";
+    // success = addAtPosition(flightsList, addAtPosFlight, position);
+    // if (success)
+    //     printDoublyLinkedList(flightsList);
+    // else
+    //     cout << "Failed to add a new flight." << endl;
+
+    return 0;
 }
