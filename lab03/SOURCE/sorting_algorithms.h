dc492fa5333adfd510fae5241b69add759ba6e6d
Phu Duc Nguyen <sillycat@duck.com>
2023-07-14 12:28:55 +0700

    Add sorting_algorithms Header file

---
+++ lab03/SOURCE/sorting_algorithms.h
@@ -0,0 +1,14 @@
+#ifndef SORTING_ALGORITHMS_H
+#define SORTING_ALGORITHMS_H
+
+#include <vector>
+
+void SelectionSort(std::vector<int>& data);
+void InsertionSort(std::vector<int>& data);
+void BubbleSort(std::vector<int>& data);
+void HeapSort(std::vector<int>& data);
+void MergeSort(std::vector<int>& data);
+void QuickSort(std::vector<int>& data);
+void RadixSort(std::vector<int>& data);
+
+#endif // SORTING_ALGORITHMS_H
